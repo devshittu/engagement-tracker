ebee26764f91b53f45e41289ac990de600c840b2
Muhammed Shittu <devshittu@gmail.com>
2025-03-22 17:51:39 +0000

    Add Layout component for service users with breadcrumb navigation

---
+++ src/app/(protected)/serviceUsers/layout.tsx
@@ -0,0 +1,17 @@
+'use client';
+import Breadcrumb from '@/components/Blocks/Breadcrumb';
+
+import React from 'react';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Breadcrumb showBack={true} />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
+
+// src/app/serviceUsers/layout.tsx
