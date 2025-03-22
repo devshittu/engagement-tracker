e491e2737e0700e1eef57c14a8a1a2a6185d8a9d
Muhammed Shittu <devshittu@gmail.com>
2025-03-22 22:36:46 +0000

    Remove ActiveSessionsPage component and associated imports

--- src/app/sessions/active/page.tsx
+++
@@ -1,5 +0,0 @@
-import ActiveSessionsInfiniteClient from '@/features/Sessions/ui/ActiveSessionsInfiniteClient';
-
-export default function ActiveSessionsPage() {
-  return <ActiveSessionsInfiniteClient />;
-}
