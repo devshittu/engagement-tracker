029bcc7c457bd472ec54bb6ad7ba24d9ba49f486
Muhammed Shittu <devshittu@gmail.com>
2025-03-22 17:51:50 +0000

    Add ServiceUsersPage component for displaying a list of service users

---
+++ src/app/(protected)/serviceUsers/page.tsx
@@ -0,0 +1,44 @@
+// import DashboardPageFrame from '@/components/Frame/DashboardPageFrame';
+// import ServiceUsersList from '../../features/ServiceUsers/ui/ServiceUsersList';
+// import Link from 'next/link';
+
+// export default function ServiceUsersPage() {
+//   return (
+//     <>
+//       <DashboardPageFrame
+//         title="Service Users"
+//         pageActions={
+//           <>
+//             {/* Add Service User Button */}
+//             <Link href="/serviceUsers/new" className="btn btn-primary">
+//               + Add Service User
+//             </Link>
+//           </>
+//         }
+//       >
+//         <ServiceUsersList />
+//       </DashboardPageFrame>
+//     </>
+//   );
+// }
+// // src/app/serviceUsers/page.tsx
+
+// src/app/serviceUsers/page.tsx
+import DashboardPageFrame from '@/components/Frame/DashboardPageFrame';
+import ServiceUsersList from '@/features/ServiceUsers/ui/ServiceUsersList';
+import Link from 'next/link';
+
+export default function ServiceUsersPage() {
+  return (
+    <DashboardPageFrame
+      title="Service Users"
+      pageActions={
+        <Link href="/serviceUsers/new" className="btn btn-primary">
+          + Add Service User
+        </Link>
+      }
+    >
+      <ServiceUsersList showReportLinks={false} />
+    </DashboardPageFrame>
+  );
+}
