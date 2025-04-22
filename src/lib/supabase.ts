a6ca23a2cb5db8674d258847516bbe5d0823b188
Muhammed Shittu <devshittu@gmail.com>
2025-04-22 19:56:10 +0100

    fix: rename from the NEXT_PUBLIC_SUPABASE_ANON_KEY to SUPABASE_ANON_KEY

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,13 +3,12 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 // Environment variables
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 // Validate required variables
 if (!supabaseUrl) throw new Error('NEXT_PUBLIC_SUPABASE_URL must be defined');
-if (!supabaseAnonKey)
-  throw new Error('NEXT_PUBLIC_SUPABASE_ANON_KEY must be defined');
+if (!supabaseAnonKey) throw new Error('SUPABASE_ANON_KEY must be defined');
 if (!supabaseServiceKey)
   throw new Error('SUPABASE_SERVICE_ROLE_KEY must be defined'); // Enforce this
 
